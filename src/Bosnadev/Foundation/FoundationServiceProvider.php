26a0d01279512c2507a3013b1eb38dcaf77c4666
Mirza Pasic <mirza.pasic@edu.fit.ba>
2015-02-14 21:12:21 +0100

    moving to L5

--- src/Bosnadev/Foundation/FoundationServiceProvider.php
+++ src/Bosnadev/Foundation/FoundationServiceProvider.php
@@ -4,43 +4,4 @@ use Illuminate\Support\ServiceProvider;
 
 class FoundationServiceProvider extends ServiceProvider {
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
-
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('bosnadev/foundation');
-	}
-
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-        $this->app['bosnadev'] = $this->app->share(function($app) {
-            return new Foundation;
-        });
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array('foundation');
-	}
-
 }
