772cb291b01f600f8ec020e45a14f8717d1940b8
Mirza Pasic <mirza.pasic@edu.fit.ba>
2015-02-14 21:15:05 +0100

    update

--- src/Bosnadev/Foundation/FoundationServiceProvider.php
+++ src/Bosnadev/Foundation/FoundationServiceProvider.php
@@ -4,4 +4,13 @@ use Illuminate\Support\ServiceProvider;
 
 class FoundationServiceProvider extends ServiceProvider {
 
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        // TODO: Implement register() method.
+    }
 }
