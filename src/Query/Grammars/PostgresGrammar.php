88422ceb57dcbf7fae3e006b125cebff655f57db
Andrew Smith <andrew@intellihr.com.au>
2015-04-09 10:23:02 +1000

    Add support for wrapping PostgreSQL JSON queries

--- src/Query/Grammars/PostgresGrammar.php
+++ src/Query/Grammars/PostgresGrammar.php
@@ -1,22 +1,76 @@
-<?php namespace Bosnadev\Database\Query\Grammars;
+<?php
+namespace Bosnadev\Database\Query\Grammars;
+
+use Illuminate\Database\Query\Builder;
+use Illuminate\Database\Query\Grammars\PostgresGrammar as LaravelPostgresGrammar;
 
 /**
  * Class PostgresGrammar
+ *
  * @package Bosnadev\Database\Query\Grammars
  */
-class PostgresGrammar extends \Illuminate\Database\Query\Grammars\PostgresGrammar {
+class PostgresGrammar extends LaravelPostgresGrammar
+{
+    /**
+     * @var array
+     */
+    protected $jsonOperators = [
+        '->',
+        '->>',
+        '#>',
+        '#>>',
+    ];
 
     /**
      * @param string $value
+     *
      * @return string
      */
     protected function wrapValue($value)
     {
-        // If quering hstore
-        if( preg_match('/\[(.*?)\]/', $value, $match) ) {
-            return (string) str_replace(array( '[', ']' ), '', $match[1]);
+        if ($value === '*') {
+            return $value;
         }
 
-        return parent::wrapValue( $value );
+        // If querying hstore
+        if (preg_match('/\[(.*?)\]/', $value, $match)) {
+            return (string)str_replace(array('[', ']'), '', $match[1]);
+        }
+
+        // If querying json column
+        foreach ($this->jsonOperators as $operator) {
+            if (stripos($value, $operator)) {
+                list($value, $key) = explode($operator, $value, 2);
+                return parent::wrapValue($value) . $operator . $key;
+            }
+        }
+
+        return parent::wrapValue($value);
     }
-} 
+
+    /**
+     * Compile a "where null" clause.
+     *
+     * @param  Builder $query
+     * @param  array   $where
+     *
+     * @return string
+     */
+    protected function whereNull(Builder $query, $where)
+    {
+        return '(' . $this->wrap($where['column']) . ') is null';
+    }
+
+    /**
+     * Compile a "where not null" clause.
+     *
+     * @param  Builder $query
+     * @param  array   $where
+     *
+     * @return string
+     */
+    protected function whereNotNull(Builder $query, $where)
+    {
+        return '(' . $this->wrap($where['column']) . ') is not null';
+    }
+}
