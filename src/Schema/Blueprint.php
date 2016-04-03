3b4631184064e228cbebd8840979baf60b21b935
Mirza Pasic <mirza.pasic@bosnadev.com>
2016-04-03 09:56:22 +0200

    added some additional pg types

--- src/Schema/Blueprint.php
+++ src/Schema/Blueprint.php
@@ -107,6 +107,127 @@ class Blueprint extends \Illuminate\Database\Schema\Blueprint
     }
 
     /**
+     * Create a new IPv4/IPv6 address (inet) column on the table.
+     *
+     * @param  string  $column
+     * @return \Illuminate\Support\Fluent
+     */
+    public function ipAddress($column)
+    {
+        return $this->addColumn('ipAddress', $column);
+    }
+    
+    /**
+     * Create a new netmask (CIDR-notation) (cidr) column on the table.
+     *
+     * @param  string  $column
+     * @return \Illuminate\Support\Fluent
+     */
+    public function netmask($column)
+    {
+        return $this->addColumn('netmask', $column);
+    }
+    
+    /**
+     * Create a new MAC address (macaddr) column on the table.
+     *
+     * @param  string  $column
+     * @return \Illuminate\Support\Fluent
+     */
+    public function macAddress($column)
+    {
+        return $this->addColumn('macAddress', $column);
+    }
+
+    /**
+     * Create a new point column on the table.
+     *
+     * @param  string  $column
+     * @return \Illuminate\Support\Fluent
+     */
+    public function point($column)
+    {
+        return $this->addColumn('point', $column);
+    }
+
+    /**
+     * Create a new line column on the table.
+     *
+     * @param  string  $column
+     * @return \Illuminate\Support\Fluent
+     */
+    public function line($column)
+    {
+        return $this->addColumn('line', $column);
+    }
+
+    /**
+     * Create a new line segment (lseg) column on the table.
+     *
+     * @param  string  $column
+     * @return \Illuminate\Support\Fluent
+     */
+    public function lineSegment($column)
+    {
+        return $this->addColumn('lineSegment', $column);
+    }
+
+    /**
+     * Create a new path column on the table.
+     *
+     * @param  string  $column
+     * @return \Illuminate\Support\Fluent
+     */
+    public function path($column)
+    {
+        return $this->addColumn('path', $column);
+    }
+
+    /**
+     * Create a new box column on the table.
+     *
+     * @param  string  $column
+     * @return \Illuminate\Support\Fluent
+     */
+    public function box($column)
+    {
+        return $this->addColumn('box', $column);
+    }
+
+    /**
+     * Create a new polygon column on the table.
+     *
+     * @param  string  $column
+     * @return \Illuminate\Support\Fluent
+     */
+    public function polygon($column)
+    {
+        return $this->addColumn('polygon', $column);
+    }
+
+    /**
+     * Create a new circle column on the table.
+     *
+     * @param  string  $column
+     * @return \Illuminate\Support\Fluent
+     */
+    public function circle($column)
+    {
+        return $this->addColumn('circle', $column);
+    }
+
+    /**
+     * Create a new money column on the table.
+     *
+     * @param  string  $column
+     * @return \Illuminate\Support\Fluent
+     */
+    public function money($column)
+    {
+        return $this->addColumn('money', $column);
+    }
+
+    /**
      * Create a new int4range column on the table.
      *
      * @param string $column
--- src/Schema/Grammars/PostgresGrammar.php
+++ src/Schema/Grammars/PostgresGrammar.php
@@ -55,6 +55,129 @@ class PostgresGrammar extends \Illuminate\Database\Schema\Grammars\PostgresGramm
     }
 
     /**
+     * Create the column definition for an IPv4 or IPv6 address.
+     *
+     * @param  \Illuminate\Support\Fluent  $column
+     * @return string
+     */
+    protected function typeIpAddress(Fluent $column)
+    {
+        return 'inet';
+    }
+    /**
+     * Create the column definition for a CIDR notation-style netmask.
+     *
+     * @param  \Illuminate\Support\Fluent  $column
+     * @return string
+     */
+    protected function typeNetmask(Fluent $column)
+    {
+        return 'cidr';
+    }
+
+    /**
+     * Create the column definition for a MAC address.
+     *
+     * @param  \Illuminate\Support\Fluent  $column
+     * @return string
+     */
+    protected function typeMacAddress(Fluent $column)
+    {
+        return 'macaddr';
+    }
+
+    /**
+     * Create the column definition for a 2D geometric point (x, y), x and y are floating-point numbers.
+     *
+     * @param  \Illuminate\Support\Fluent  $column
+     * @return string
+     */
+    protected function typePoint(Fluent $column)
+    {
+        return 'point';
+    }
+
+    /**
+     * Create the column definition for a line represented as a standard linear equation Ax + By + C = 0.
+     *
+     * @param  \Illuminate\Support\Fluent  $column
+     * @return string
+     */
+    protected function typeLine(Fluent $column)
+    {
+        return 'line';
+    }
+
+    /**
+     * Create the column definition for a line segment represented by two points (x1, y1), (x2, y2).
+     *
+     * @param  \Illuminate\Support\Fluent  $column
+     * @return string
+     */
+    protected function typeLineSegment(Fluent $column)
+    {
+        return 'lseg';
+    }
+
+    /**
+     * Create the column definition for a path represented as a list of points (x1, y1), (x2, y2), ..., (xn, yn).
+     *
+     * @param  \Illuminate\Support\Fluent  $column
+     * @return string
+     */
+    protected function typePath(Fluent $column)
+    {
+        return 'path';
+    }
+
+    /**
+     * Create the column definition for a box represented by opposite corners of the box as points (x1, y1), (x2, y2).
+     *
+     * @param  \Illuminate\Support\Fluent  $column
+     * @return string
+     */
+    protected function typeBox(Fluent $column)
+    {
+        return 'box';
+    }
+
+    /**
+     * Create the column definition for a polygon represented by a list of points (vertices of the polygon).
+     *
+     * @param  \Illuminate\Support\Fluent  $column
+     * @return string
+     */
+    protected function typePolygon(Fluent $column)
+    {
+        return 'polygon';
+    }
+
+    /**
+     * Create the column definition for a circle represented by a center point and a radius <(x, y), r>
+     *
+     * @param  \Illuminate\Support\Fluent  $column
+     * @return string
+     */
+    protected function typeCircle(Fluent $column)
+    {
+        return 'circle';
+    }
+
+    /**
+     * Create the column definition for storing amounts of currency with a fixed fractional precision.
+     *
+     * This will store values in the range of: -92233720368547758.08 to +92233720368547758.07. (92 quadrillion).
+     * Output is locale-sensitive, see lc_monetary setting of PostgreSQL instance/s.
+     *
+     * @param  \Illuminate\Support\Fluent  $column
+     * @return string
+     */
+    protected function typeMoney(Fluent $column)
+    {
+        return 'money';
+    }
+
+    /**
      * Create the column definition for an int4range type.
      *
      * @param Fluent $column
